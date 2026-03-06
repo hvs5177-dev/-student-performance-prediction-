53e1d8f084aa72875af693f5b451beef320c3d88
hvs5177-dev <hvs5177@gmail.com>
2026-03-06 16:13:29 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,63 @@
+import streamlit as st
+import numpy as np
+from tensorflow.keras.models import load_model
+
+# Load model
+
+marks_model = load_model("student_marks_prediction_model.h5")
+
+st.title("Student Performance Prediction System")
+
+st.write("Enter student details to predict performance")
+
+# Inputs
+
+age = st.number_input("Age", 10, 25, 18)
+study_hours = st.number_input("Study Hours Per Day", 0.0, 12.0)
+attendance = st.number_input("Attendance Percentage", 0.0, 100.0)
+
+math_score = st.number_input("Math Score", 0.0, 100.0)
+science_score = st.number_input("Science Score", 0.0, 100.0)
+english_score = st.number_input("English Score", 0.0, 100.0)
+previous_score = st.number_input("Previous Year Score", 0.0, 100.0)
+
+if st.button("Predict Performance"):
+
+```
+input_data = np.array([[
+    age,
+    study_hours,
+    attendance,
+    math_score,
+    science_score,
+    english_score,
+    previous_score
+]])
+
+input_data = input_data.reshape(1, input_data.shape[1], 1)
+
+prediction = marks_model.predict(input_data)
+
+predicted_marks = float(prediction[0][0])
+
+lower = round(predicted_marks - 4)
+upper = round(predicted_marks + 4)
+
+if predicted_marks >= 40:
+    result = "PASS"
+else:
+    result = "FAIL"
+
+if predicted_marks >= 75:
+    risk = "Low Risk"
+elif predicted_marks >= 50:
+    risk = "Medium Risk"
+else:
+    risk = "High Risk"
+
+st.subheader("Prediction Result")
+st.write("Predicted Marks:", round(predicted_marks,2))
+st.write("Marks Range:", lower, "-", upper)
+st.write("Prediction:", result)
+st.write("Risk Level:", risk)
+```
